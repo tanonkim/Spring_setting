3a3df5f4ce007ab5e2696848bba945d2a81b2375
tanonkim <robbins4bos@gmail.com>
2022-07-04 18:29:32 +0900

    add : HelloWorld Bean

---
+++ src/main/java/com/example/restfulwebservice1/HelloWorldBean.java
@@ -0,0 +1,13 @@
+package com.example.restfulwebservice1;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//lombok
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldBean {
+    private String message;
+}
--- src/main/java/com/example/restfulwebservice1/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice1/HelloWorldController.java
@@ -8,7 +8,12 @@ public class HelloWorldController {
     // GET
     // /hello-world (end-point)
     @GetMapping(path = "/hello-world")
-    public String helloWorld(){
+    public String helloWorld() {
         return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {     // HelloWorldBean : 반환값
+        return new HelloWorldBean("Hello World"); // option + Enter : 해결단축키
     }
 }
