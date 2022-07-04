c232b06585690a04ea6d92209b7164da792c3a86
tanonkim <robbins4bos@gmail.com>
2022-07-04 16:39:12 +0900

    initial_setting

---
+++ src/main/java/com/example/restfulwebservice1/RestfulWebService1Application.java
@@ -0,0 +1,13 @@
+package com.example.restfulwebservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebService1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulWebService1Application.class, args);
+    }
+
+}
