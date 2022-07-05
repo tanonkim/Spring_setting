7edb6d26460566083dbc05d2faf040a84fbc907f
tanonkim <robbins4bos@gmail.com>
2022-07-05 18:51:08 +0900

    add : Version Control by using URI for REST API

--- src/main/java/com/example/restfulwebservice1/user/User.java
+++ src/main/java/com/example/restfulwebservice1/user/User.java
@@ -1,8 +1,9 @@
 package com.example.restfulwebservice1.user;
 
-import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
@@ -10,8 +11,9 @@ import java.util.Date;
 
 @Data
 @AllArgsConstructor
-//@JsonIgnoreProperties(value = {"password", "ssn"})
-@JsonFilter("UserInfo")
+@NoArgsConstructor
+@JsonIgnoreProperties(value = {"password", "ssn"})
+//@JsonFilter("UserInfo")
 public class User {
     private Integer id;
 
---
+++ src/main/java/com/example/restfulwebservice1/user/UserV2.java
@@ -0,0 +1,17 @@
+package com.example.restfulwebservice1.user;
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+//@JsonIgnoreProperties(value = {"password", "ssn"})
+@JsonFilter("UserInfoV2")
+public class UserV2 extends User {
+    private String grade;
+
+}
+
