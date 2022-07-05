e447f6c25a3feb535c54738f63185460585be475
tanonkim <robbins4bos@gmail.com>
2022-07-05 16:43:48 +0900

    add : Localizing

--- src/main/java/com/example/restfulwebservice1/RestfulWebService1Application.java
+++ src/main/java/com/example/restfulwebservice1/RestfulWebService1Application.java
@@ -2,11 +2,23 @@ package com.example.restfulwebservice1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulWebService1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(RestfulWebService1Application.class, args);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.KOREA);
+        return localeResolver;
     }
 }
