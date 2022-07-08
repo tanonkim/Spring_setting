415fd3ec9b999c4156ae79122f198a18b709fb76
tanonkim <robbins4bos@gmail.com>
2022-07-08 11:19:49 +0900

    add : Inquiring  User List with JPA

--- src/main/java/com/example/restfulwebservice1/user/UserJpaController.java
+++ src/main/java/com/example/restfulwebservice1/user/UserJpaController.java
@@ -1,11 +1,15 @@
 package com.example.restfulwebservice1.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/jpa")
@@ -14,9 +18,24 @@ public class UserJpaController {
     private UserRepository userRepository;
 
     @GetMapping("/users")
-    public List<Userr> retrieveAllUser() {
+    public List<Userr> retrieveAllUsers() {
         return userRepository.findAll();
     }
 
+    @GetMapping("/users/{id}")
+    public EntityModel<Userr> retrieveUser(@PathVariable int id) {
+        Optional<Userr> user = userRepository.findById(id);
+
+        if (!user.isPresent()) {
+            throw new UserNotFoundException(String.format("ID[%S] not found", id));
+        }
+
+        EntityModel<Userr> model = EntityModel.of(user.get());
+        WebMvcLinkBuilder linkTo = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).retrieveAllUsers());
+        model.add(linkTo.withRel("all-users"));
+
+        return model;
+
+    }
 
 }
